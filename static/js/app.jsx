38b286c92418e378d702e69cc2d260021d37d2dc
Jessica Chapdelaine <jessicachap223@gmail.com>
2021-05-19 18:01:58 +0000

    working on navbar showing on the correct pages whether user

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -3,11 +3,17 @@ function App() {
 
 
     return (
-        <ReactRouterDOM.BrowserRouter>
-            <nav>
+        <ReactRouterDOM.BrowserRouter> 
+            {/* maybe add switch */}
+            <nav id="logout">
                 <ReactRouterDOM.Link to="/">Homepage | </ReactRouterDOM.Link>
                 <ReactRouterDOM.Link to="/login">Login | </ReactRouterDOM.Link>
                 <ReactRouterDOM.Link to="/signup">Signup | </ReactRouterDOM.Link>
+                <hr></hr>
+            </nav>
+            <nav id="login">
+                {/* the two below need to only show when logged in - store in state
+                check state when renders component done in this file */}
                 <ReactRouterDOM.Link to="/favorites">Favorites | </ReactRouterDOM.Link>
                 <ReactRouterDOM.Link to="/logout">Logout</ReactRouterDOM.Link>
                 <hr></hr>
