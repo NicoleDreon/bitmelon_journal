d9f3367cff2dfe8f92726531c352e61072312fa0
Jessica Chapdelaine <jessicachap223@gmail.com>
2021-05-19 14:03:08 +0000

    Created working nav bar, updating login feature on server.py

--- static/js/uiComponents.jsx
+++ static/js/uiComponents.jsx
@@ -21,27 +21,3 @@ function Login(props) {
     );
 }
 
-// function Navbar(props) {
-//     const { logo, brand, children, className } = props;
-  
-//     const navLinks = children.map((el, i) => {
-//       return (
-//         <div key={i} className="nav-item">
-//           {el}
-//         </div>
-//       );
-//     });
-  
-//     return (
-//       <nav className={`navbar ${className || ""}`}>
-//         <ReactRouterDOM.Link
-//           to="/"
-//           className="havbar-brand d-flex justify-content-center">
-//           <img src={logo} height="30" />
-//           <span>{brand}</span>
-//         </ReactRouterDOM.Link>
-  
-//         <section className="d-flex justify-content-center">{navLinks}</section>
-//       </nav>
-//     );
-//   }
