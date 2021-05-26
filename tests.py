7d2d70d5404707c4e3b9f7cd4524c9e04567b59a
smatloc1 <optimizequality@gmail.com>
2021-05-26 16:52:58 +0000

    updated tests.py

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from server import app, journal
-from test_model import Journal_entry, Melon, Memory, User, connect_to_db, db 
+from model import Journal_entry, Melon, Memory, User, connect_to_db, db 
 from datetime import date
 import json 
 
@@ -15,20 +15,20 @@ def test_data():
     Melon.query.delete()
     
     
-    # Add sample User, melon, journal_entrY. and memory
-    carol = User(user_name='CarolT', password='t2455xx', email='carolt@gmail.com')
+    # Add sample data for user, melon, journal_entrY. and memory
+    sameea = User(user_name='Sameea', password='123', email='sameea@gmail.com')
     pepino = Melon(melon_name='Pepino – Solanum muricatum', melon_img='https://res.cloudinary.com/dpapvtab4/image/upload/v1621265037/melons/Pepino_Solanum_muricatum_y3a5nu.png', 
         description='Technically, the Pepino (sweet cucumber) isn’t a melon. It is not a member of the gourd family, Cucurbitaceae. It isn’t even in the same order as melons, either. That said, culinarily, it is treated like a melon. They look like honeydew and have smooth, mottled skin.')
 
-    db.session.add_all([carol, pepino])
+    db.session.add_all([sameea, pepino])
     db.session.commit() 
     
-    carolsjournal = Journal_entry(melon_id=pepino.melon_id, user_id=carol.user_id, title=pepino.melon_name, rating=5, entry='The best melon I have ever tasted', favorite=True)
+    sameeasjournal = Journal_entry(melon_id=pepino.melon_id, user_id=sameea.user_id, title=pepino.melon_name, rating=5, entry='The best melon I have ever tasted', favorite=True)
     
-    db.session.add_all([carolsjournal])
+    db.session.add_all([sameeasjournal])
     db.session.commit()
 
-    juicymemory = Memory(journal_id=carolsjournal.journal_id, memory='Still the best melon I ever tasted', location='Upper P, Michigan', friend='Sherry', date=date.today())    
+    juicymemory = Memory(journal_id=sameeasjournal.journal_id, memory='Still the best melon I ever tasted', location='Upper P, Michigan', friend='Sherry', date=date.today())    
     
     db.session.add_all([juicymemory])
     db.session.commit()
@@ -39,7 +39,6 @@ class FlaskTestsAllMelons(TestCase):
 
     def setUp(self):
         """Stuff to do before every test."""
-#WORKING
 
         self.client = app.test_client()
         app.config['TESTING'] = True
@@ -50,16 +49,18 @@ class FlaskTestsAllMelons(TestCase):
         test_data()   
 
 
-    # def test_viewallmelons(self):
-    #     """  Can we view all melons in the database?  """
-#TODO NOT WORKING###
+    def test_viewallmelons(self):
+        """  Can we view all melons in the database?  """
 
-    #     result = self.client.get('/allmelons.json')
-    #     self.assertIn(b'Casaba', result.data)
+        data={'melon_name': 'Pepino – Solanum muricatum', 'description':'Technically, the Pepino (sweet cucumber) isn’t a melon. It is not a member of the gourd family, Cucurbitaceae.' 
+                            'It isn’t even in the same order as melons, either. That said, culinarily, it is treated like a melon. They look like honeydew and have smooth, mottled skin.',
+                            'url':'https://res.cloudinary.com/dpapvtab4/image/upload/v1621265037/melons/Pepino_Solanum_muricatum_y3a5nu.png'} 
+        data= json.dumps(data)
+        result = self.client.post('/allmelons.json', data=data, follow_redirects=True, content_type='application/json')
+        self.assertIn(b'sweet cucumber', result.data)
 
     def tearDown(self):
         """Do at end of every test."""
-#WORKING
 
         db.session.close()
         db.drop_all()
@@ -67,9 +68,9 @@ class FlaskTestsAllMelons(TestCase):
 
 
 class FlaskTestsJournalEntry(TestCase):
+   
     def setUp(self):
         """Stuff to do before every test."""
-#WORKING
 
         self.client = app.test_client()
         app.config['TESTING'] = True
@@ -80,98 +81,74 @@ class FlaskTestsJournalEntry(TestCase):
         test_data()
 
     def test_journalentry(self):
-        """  Can we add a journal entry and/or memory to the database?  """
+        """  Can we add a journal entry to the database?  """
       
         data={'melon_name': 'pepino.melon_name', 'rating':5, 
                                 'entry':'The best melon I have ever tasted', 'favorite':True,
-                                'email': 'carolt@gmail.com'} 
+                                'email': 'sameea@gmail.com'} 
         data= json.dumps(data)
         result = self.client.post('/journal.json', data=data, follow_redirects=True, content_type='application/json')
-        self.assertIn(b'Memory Location', result.data)
+        self.assertIn(b'Tasting Notes', result.data)
 
 
     def tearDown(self):
         """Do at end of every test."""
-#WORKING
 
         db.session.close()
         db.drop_all()
     
 
+class FlaskTestsMemory(TestCase):
+   
+    def setUp(self):
+        """Stuff to do before every test."""
 
-# class FlaskTestsLoggedIn(TestCase):
-#     """Flask tests with user logged in to session."""
+        self.client = app.test_client()
+        app.config['TESTING'] = True
 
-#     def setUp(self):
-#         """Stuff to do before every test."""
+        connect_to_db(app, "postgresql:///testdb")
 
-#         app.config['TESTING'] = True
-#         app.config['SECRET_KEY'] = 'key'
-#         self.client = app.test_client()
+        db.create_all()
+        test_data()
 
-#         with self.client as c:
-#             with c.session_transaction() as sess:
-#                 sess['user_id'] = 1
-
-#     def test_important_page(self):
-#         """Test important page."""
-
-#         result = self.client.get("/important")
-#         self.assertIn(b"You are a valued user", result.data)
-
-
-# class FlaskTestsLoggedOut(TestCase):
-#     """Flask tests with user logged in to session."""
-
-#     def setUp(self):
-#         """Stuff to do before every test."""
-
-#         app.config['TESTING'] = True
-#         self.client = app.test_client()
-
-#     def test_important_page(self):
-#         """Test that user can't see important page when logged out."""
-
-#         result = self.client.get("/important", follow_redirects=True)
-#         self.assertNotIn(b"You are a valued user", result.data)
-#         self.assertIn(b"You must be logged in", result.data)
+    def test_memory(self):
+        """  Can we add a memory to the database?  """
+      
+        data={'journal': 'sameeasjournal.journal_id', 'memory':'Still the best melon I ever tasted!', 'location':'Upper P, Michigan', 'friend':'Sherry', 'date':date.today()}
+        data= json.dumps(data)
+        result = self.client.post('/memory.json', data=data, follow_redirects=True, content_type='application/json')
+        self.assertIn(b'Friend', result.data)
 
 
-# class FlaskTestsLogInLogOut(TestCase):  # Bonus example. Not in lecture.
-#     """Test log in and log out."""
+    def tearDown(self):
+        """Do at end of every test."""
 
-#     def setUp(self):
-#         """Before every test"""
+        db.session.close()
+        db.drop_all()
 
-#         app.config['TESTING'] = True
-#         self.client = app.test_client()
+class FlaskTestsShowJournal(TestCase):
+    def setUp(self):
+        """Stuff to do before every test."""
 
-#     def test_login(self):
-#         """Test log in form.
+        self.client = app.test_client()
+        app.config['TESTING'] = True
 
-#         Unlike login test above, 'with' is necessary here in order to refer to session.
-#         """
+        connect_to_db(app, "postgresql:///testdb")
 
-#         with self.client as c:
-#             result = c.post('/login',
-#                             data={'user_id': '42', 'password': 'abc'},
-#                             follow_redirects=True
-#                             )
-#             self.assertEqual(session['user_id'], '42')
-#             self.assertIn(b"You are a valued user", result.data)
+        db.create_all()
+        test_data()
 
-#     def test_logout(self):
-#         """Test logout route."""
+    def test_showjournal(self):
+        """Can we retrieve the details of a user journal?"""
 
-#         with self.client as c:
-#             with c.session_transaction() as sess:
-#                 sess['user_id'] = '42'
+        result = self.client.get('/showjournals.json')
+        self.assertIn(b"Journal", result.data)
 
-#             result = self.client.get('/logout', follow_redirects=True)
+    def tearDown(self):
+        """Do at end of every test."""
 
-#             self.assertNotIn(b'user_id', session)
-#             self.assertIn(b'Logged Out', result.data)
-
+        db.session.close()
+        db.drop_all()
 
 
 if __name__ == "__main__":
